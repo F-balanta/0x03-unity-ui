b06bab3e96aa5406eb8c2b087815bae88d2b6142
F-balanta <jf.balanta@hotmail.com>
2022-05-12 10:01:50 -0500

    Task 12 Finished

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,7 +9,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayMaze()
     {
-        /*if (colorblindMode.isOn)
+        if (colorblindMode.isOn)
         {
             trapMat.color = new Color32(255, 112, 0, 1);
             goalMat.color = Color.blue;
@@ -18,7 +18,7 @@ public class MainMenu : MonoBehaviour
         {
             trapMat.color = Color.red;
             goalMat.color = Color.green;
-        }*/
+        }
         SceneManager.LoadScene("maze");
     }
     public void QuitMaze()
